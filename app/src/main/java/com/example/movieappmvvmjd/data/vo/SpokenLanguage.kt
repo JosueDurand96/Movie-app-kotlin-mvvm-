ef971b38abe99e45ebe3bfabe6a0416dbf04a10c
JosueDurand96 <josue.durand8@gmail.com>
2019-11-04 22:44:21 -0500

    pojo

--- app/src/main/java/com/example/movieappmvvmjd/data/vo/SpokenLanguage.kt
+++ app/src/main/java/com/example/movieappmvvmjd/data/vo/SpokenLanguage.kt
@@ -3,7 +3,7 @@ package com.example.movieappmvvmjd.data.vo
 
 import com.google.gson.annotations.SerializedName
 
-data class SpokenLanguage(
+data class  SpokenLanguage(
     @SerializedName("iso_639_1")
     val iso6391: String,
     @SerializedName("name")
