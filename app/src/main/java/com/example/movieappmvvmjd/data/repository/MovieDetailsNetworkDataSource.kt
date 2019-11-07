9280bbceb2ed0f4f0b7a7bc688d43980cf1b430e
JosueDurand <josue.durand8@gmail.com>
2019-11-07 13:56:39 -0500

    MovieDetailsNetworkDataSource

--- app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDetailsNetworkDataSource.kt
+++ app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDetailsNetworkDataSource.kt
@@ -1,4 +1,15 @@
 package com.example.movieappmvvmjd.data.repository
 
-class MovieDetailsNetworkDataSource {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsNetworkDataSource(private val apiService:TheMovieDBInterface, private val compositeDisposable: CompositeDisposable) {
+
+    private val _networkState = MutableLiveData<NetworkState>()
+    val networkState:LiveData<NetworkState>
+        get() = _networkState
+
+
 }
