da92f714863cafaf8adb521225c317b7c0211f21
JosueDurand <josue.durand8@gmail.com>
2019-11-07 14:16:50 -0500

    viewmodel

--- app/src/main/java/com/example/movieappmvvmjd/single_movie_details/Repository/MovieDetailsRepository.kt
+++ app/src/main/java/com/example/movieappmvvmjd/single_movie_details/Repository/MovieDetailsRepository.kt
@@ -1,4 +1,24 @@
 package com.example.movieappmvvmjd.single_movie_details.Repository
 
-class MovieDetailsRepository {
+import androidx.lifecycle.LiveData
+import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import com.example.movieappmvvmjd.data.repository.MovieDetailsNetworkDataSource
+import com.example.movieappmvvmjd.data.repository.NetworkState
+import com.example.movieappmvvmjd.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository(private val apiService:TheMovieDBInterface) {
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSigleMovieDetails(compositeDisposable: CompositeDisposable,movieId:Int):LiveData<MovieDetails>{
+        movieDetailsNetworkDataSource= MovieDetailsNetworkDataSource(apiService,compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieResponse
+    }
+
+    fun getMovieDetailsNetworkState():LiveData<NetworkState>{
+        return movieDetailsNetworkDataSource.networkState
+    }
+
 }
---
+++ app/src/main/java/com/example/movieappmvvmjd/single_movie_details/ViewModel/SingleMovieViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.movieappmvvmjd.single_movie_details.ViewModel
+
+class SingleMovieViewModel {
+}
