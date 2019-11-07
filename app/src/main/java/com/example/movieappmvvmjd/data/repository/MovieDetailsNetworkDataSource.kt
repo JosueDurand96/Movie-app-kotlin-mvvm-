80e7f79b07495f6f9012b32acd3018336cfe2bea
JosueDurand <josue.durand8@gmail.com>
2019-11-07 13:59:14 -0500

    MovieDetailsNetworkDataSource

--- app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDetailsNetworkDataSource.kt
+++ app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDetailsNetworkDataSource.kt
@@ -3,6 +3,7 @@ package com.example.movieappmvvmjd.data.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import com.example.movieappmvvmjd.data.vo.MovieDetails
 import io.reactivex.disposables.CompositeDisposable
 
 class MovieDetailsNetworkDataSource(private val apiService:TheMovieDBInterface, private val compositeDisposable: CompositeDisposable) {
@@ -10,6 +11,14 @@ class MovieDetailsNetworkDataSource(private val apiService:TheMovieDBInterface, 
     private val _networkState = MutableLiveData<NetworkState>()
     val networkState:LiveData<NetworkState>
         get() = _networkState
+
+    private val _downloadedMovieDetailsResponse = MutableLiveData<MovieDetails>()
+    val downloadedMovieResponse: LiveData<MovieDetails>
+        get() = _downloadedMovieDetailsResponse
+
+    fun fetchMovieDetails(movieId:Int){
+
+    }
 
 
 }
