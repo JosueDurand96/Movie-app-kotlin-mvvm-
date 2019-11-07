8104cd2bd9b6b9f6f8df9b2ff4e6b92ca7912035
JosueDurand <josue.durand8@gmail.com>
2019-11-07 14:04:15 -0500

    MovieDetailsNetworkDataSource

--- app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDetailsNetworkDataSource.kt
+++ app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDetailsNetworkDataSource.kt
@@ -4,20 +4,44 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
 import com.example.movieappmvvmjd.data.vo.MovieDetails
+import io.reactivex.Scheduler
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.lang.Exception
 
-class MovieDetailsNetworkDataSource(private val apiService:TheMovieDBInterface, private val compositeDisposable: CompositeDisposable) {
+class MovieDetailsNetworkDataSource(
+    private val apiService: TheMovieDBInterface,
+    private val compositeDisposable: CompositeDisposable
+) {
 
     private val _networkState = MutableLiveData<NetworkState>()
-    val networkState:LiveData<NetworkState>
+    val networkState: LiveData<NetworkState>
         get() = _networkState
 
     private val _downloadedMovieDetailsResponse = MutableLiveData<MovieDetails>()
     val downloadedMovieResponse: LiveData<MovieDetails>
         get() = _downloadedMovieDetailsResponse
 
-    fun fetchMovieDetails(movieId:Int){
+    fun fetchMovieDetails(movieId: Int) {
+        _networkState.postValue(NetworkState.LOADING)
 
+        try {
+            compositeDisposable.add(
+                apiService.getMovieDetails(movieId)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            _downloadedMovieDetailsResponse.postValue(it)
+                        },
+                        {
+
+                        }
+
+                    )
+            )
+        }catch (e:Exception){
+
+        }
     }
 
 
