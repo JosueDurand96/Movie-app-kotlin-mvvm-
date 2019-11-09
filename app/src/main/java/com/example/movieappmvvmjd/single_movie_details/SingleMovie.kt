3485dcd3801f405d576aadcba6d8d471438b491a
JosueDurand96 <josue.durand8@gmail.com>
2019-11-09 00:24:54 -0500

    Single movie

--- app/src/main/java/com/example/movieappmvvmjd/single_movie_details/SingleMovie.kt
+++ app/src/main/java/com/example/movieappmvvmjd/single_movie_details/SingleMovie.kt
@@ -3,11 +3,23 @@ package com.example.movieappmvvmjd.single_movie_details
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movieappmvvmjd.R
+import com.example.movieappmvvmjd.data.api.TheMovieDBClient
+import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import com.example.movieappmvvmjd.single_movie_details.Repository.MovieDetailsRepository
+import com.example.movieappmvvmjd.single_movie_details.ViewModel.SingleMovieViewModel
 
 class SingleMovie : AppCompatActivity() {
+
+    private lateinit var viewModel: SingleMovieViewModel
+    private lateinit var movieRepository: MovieDetailsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_movie)
+        val movieId: Int = 1
+
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+
+
     }
 }
