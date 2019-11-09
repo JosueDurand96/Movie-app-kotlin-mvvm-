0520e7be53443d1b56894d132c9ef8c35c249fa6
JosueDurand96 <josue.durand8@gmail.com>
2019-11-09 00:34:45 -0500

    viewmodel

--- app/src/main/java/com/example/movieappmvvmjd/single_movie_details/SingleMovie.kt
+++ app/src/main/java/com/example/movieappmvvmjd/single_movie_details/SingleMovie.kt
@@ -2,6 +2,9 @@ package com.example.movieappmvvmjd.single_movie_details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.movieappmvvmjd.R
 import com.example.movieappmvvmjd.data.api.TheMovieDBClient
 import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
@@ -20,6 +23,17 @@ class SingleMovie : AppCompatActivity() {
 
         val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
 
+        movieRepository = MovieDetailsRepository(apiService)
 
+        viewModel = getViewModel(movieId)
+    }
+
+    private fun getViewModel(movieId: Int): SingleMovieViewModel {
+        return ViewModelProviders.of(this,object :ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return SingleMovieViewModel(movieRepository,movieId) as T
+            }
+        })[SingleMovieViewModel::class.java]
     }
 }
