b51522e1438a9663c7e696f309cf0820972d3faa
JosueDurand96 <josue.durand8@gmail.com>
2019-11-10 02:30:29 -0500

    moviedatasourcefactory

--- app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDataSourceFactory.kt
+++ app/src/main/java/com/example/movieappmvvmjd/data/repository/MovieDataSourceFactory.kt
@@ -1,4 +1,23 @@
 package com.example.movieappmvvmjd.data.repository
 
-class MovieDataSourceFactory {
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import com.example.movieappmvvmjd.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDataSourceFactory(
+    private val apiService: TheMovieDBInterface,
+    private val compositeDisposable: CompositeDisposable
+) : DataSource.Factory<Int, Movie>() {
+
+    val moviesLiveDataSource = MutableLiveData<MovieDataSource>()
+
+    override fun create(): DataSource<Int, Movie> {
+        val movieDataSource = MovieDataSource(apiService, compositeDisposable)
+        moviesLiveDataSource.postValue(movieDataSource)
+        return movieDataSource
+    }
+
 }
