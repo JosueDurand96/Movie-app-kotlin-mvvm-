39ad53a4f951398ab107e7d16a3396b5757efb11
JosueDurand96 <josue.durand8@gmail.com>
2019-11-10 12:28:59 -0500

    activity and use recycler

--- app/src/main/java/com/example/movieappmvvmjd/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/example/movieappmvvmjd/ui/popular_movie/MainActivity.kt
@@ -2,17 +2,47 @@ package com.example.movieappmvvmjd.ui.popular_movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.movieappmvvmjd.R
+import com.example.movieappmvvmjd.data.api.TheMovieDBClient
+import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainActivityViewModel
 
-    lateinit var  moviePageListRepository: MoviePageListRepository
+    lateinit var moviePageListRepository: MoviePageListRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+        moviePageListRepository = MoviePageListRepository(apiService)
+        viewModel = getViewModel()
+        val movieAdapter = PopularMoviePageListAdapter(this)
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType: Int = movieAdapter.getItemViewType(position)
+                if (viewType == movieAdapter.MOVIE_VIEW_TYPE) return 1
+                else return 3
+            }
+        }
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+    }
 
+    private fun getViewModel(): MainActivityViewModel {
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(moviePageListRepository) as T
+            }
+        })[MainActivityViewModel::class.java]
     }
 }
