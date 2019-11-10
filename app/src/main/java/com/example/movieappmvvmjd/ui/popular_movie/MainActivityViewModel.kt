8022c8e35ac64598d65308e79300eba4828f9b49
JosueDurand96 <josue.durand8@gmail.com>
2019-11-10 02:46:49 -0500

    viewmodel

--- app/src/main/java/com/example/movieappmvvmjd/ui/popular_movie/MainActivityViewModel.kt
+++ app/src/main/java/com/example/movieappmvvmjd/ui/popular_movie/MainActivityViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.movieappmvvmjd.ui.popular_movie
 
-class MainActivityViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.paging.PagedList
+import com.example.movieappmvvmjd.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MainActivityViewModel(private val movieRepository: MoviePageListRepository) : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    val moviePageList: LiveData<PagedList<Movie>> by lazy {
+        movieRepository.fetchLiveMoviePagedList(compositeDisposable)
+    }
+
+
 }
