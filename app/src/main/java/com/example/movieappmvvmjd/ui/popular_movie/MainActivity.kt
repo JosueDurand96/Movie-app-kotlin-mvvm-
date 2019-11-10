5a2830ed10e9a3b2fe60d2a1ce49b809b5a91429
JosueDurand96 <josue.durand8@gmail.com>
2019-11-10 12:54:39 -0500

    activity

--- app/src/main/java/com/example/movieappmvvmjd/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/example/movieappmvvmjd/ui/popular_movie/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.movieappmvvmjd.ui.popular_movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.movieappmvvmjd.R
 import com.example.movieappmvvmjd.data.api.TheMovieDBClient
 import com.example.movieappmvvmjd.data.api.TheMovieDBInterface
+import com.example.movieappmvvmjd.data.repository.NetworkState
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,6 +38,23 @@ class MainActivity : AppCompatActivity() {
         rv_movie_list.layoutManager = gridLayoutManager
         rv_movie_list.setHasFixedSize(true)
         rv_movie_list.adapter = movieAdapter
+
+        viewModel.moviePageList.observe(this, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar_popular.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+
+            if (!viewModel.listIsEmpty()){
+                movieAdapter.setNetworkState(it)
+            }
+        })
+
     }
 
     private fun getViewModel(): MainActivityViewModel {
